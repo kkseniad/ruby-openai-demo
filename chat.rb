4cd6b7c2cb5925878edd534e6e15003998003c73
Kseniia Kormalova <kormalovak@gmail.com>
2025-03-30 17:32:30 +0000

    initial

--- chat.rb
+++ chat.rb
@@ -1,1 +1,32 @@
-# Write your solution here!
+require "openai"
+require "dotenv/load"
+
+client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
+
+puts "Hello! How can I help you today?"
+50.times {print '-'} 
+
+user_messege = gets.chomp
+
+
+# Prepare an Array of previous messages
+message_list = [
+  {
+    "role" => "system",
+    "content" => "You are a helpful assistant."
+  },
+  {
+    "role" => "user",
+    "content" => "Hello! What are the best spots for pizza in Chicago?"
+  }
+]
+
+# Call the API to get the next message from GPT
+api_response = client.chat(
+  parameters: {
+    model: "gpt-3.5-turbo",
+    messages: message_list
+  }
+)
+
+pp api_response
